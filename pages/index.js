39bbb90431400ef41a37fd33d117a1769bffcdbb
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2021-09-27 12:26:13 -0300

    fix index page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
 
-import Layout from '../components/themes/base/Layout'
-import Card from '../components/themes/base/Card'
-import {
-  ARTICLE_SETTINGS,
-  ARTICLE_COLLECTIONS
-} from '../client/queries'
-import client from '../client/client'
-import Link from 'next/link'
+import Layout from "../components/themes/base/Layout";
+import Card from "../components/themes/base/Card";
+import { ARTICLE_SETTINGS, ARTICLE_COLLECTIONS } from "../client/queries";
+import client from "../client/client";
+import Link from "next/link";
 
-export default function Home({site, collections}) {
+export default function Home({ site, collections }) {
   return (
     <div className="page">
       <Head>
@@ -21,60 +18,62 @@ export default function Home({site, collections}) {
 
       <Layout className="py-12 sm:px-6 md:px-64 bg-gray-100" site={site}>
         <div className="md:grid md:grid-cols-3 md:gap-x-4 md:gap-y-10">
-          {
-            collections.map((card)=>(
+          {collections.map((card) => (
+            <div className="m-4" item key={card.id}>
+              <Card
+                title={
+                  <Link href={`/${card.slug}`}>
+                    <a className={"hover:underline"} color={"primary"}>
+                      {card.icon && (
+                        <div className="flex items-center justify-center h-12 w-12 rounded-md bg-white bg-opacity-10">
+                          <Image
+                            src={`${process.env.HOST}${card.icon}`}
+                            width="200"
+                            height="200"
+                          />
+                        </div>
+                      )}
 
-              <div className="m-4" item key={card.id}>
-                <Card
-                  title={
-                    <Link href={`/${card.slug}`}>
-                      <a
-                        className={'hover:underline'}
-                        color={'primary'}
-                      >
-                        {card.icon && (
-                          <div className="flex items-center justify-center h-12 w-12 rounded-md bg-white bg-opacity-10">
-                            <Image src={`${process.env.HOST}${card.icon}`} width="200" height="200"/>
-                          </div>
-                        )}
-      
-                        <p className="mt-2 text-base">{card.title}</p>
-                      </a>
-                    </Link>
-                  }
-                  //description={truncateOnWord(card.description, 120)}
-                  description={card.description}
-                ></Card>
-              </div>
-            ))
-          }
+                      <p className="mt-2 text-base">{card.title}</p>
+                    </a>
+                  </Link>
+                }
+                //description={truncateOnWord(card.description, 120)}
+                description={card.description}
+              ></Card>
+            </div>
+          ))}
         </div>
       </Layout>
-
-      
     </div>
-  )
+  );
 }
-
 
 // This function gets called at build time
 export async function getStaticProps() {
   // Call an external API endpoint to get posts
 
-  const res = await client(ARTICLE_SETTINGS, {"domain":"dev"})
-  const site = await res.json()
+  const res = await client(ARTICLE_SETTINGS, { domain: process.env.SITE });
+  const site = await res.json();
 
-  const postsRes = await client(ARTICLE_COLLECTIONS, {"domain":"dev","lang":"en"}) 
-  
-  const {data: {helpCenter: {collections}}} = await postsRes.json()
+  const postsRes = await client(ARTICLE_COLLECTIONS, {
+    domain: process.env.SITE,
+    lang: "en",
+  });
 
-  console.log(JSON.stringify(collections))
+  const {
+    data: {
+      helpCenter: { collections },
+    },
+  } = await postsRes.json();
+
+  console.log(JSON.stringify(collections));
   // By returning { props: { posts } }, the Blog component
   // will receive `posts` as a prop at build time
   return {
     props: {
       site,
-      collections 
+      collections,
     },
-  }
+  };
 }
